23785d426efc249ee4c5d4adcd496dd0af5f4c65
ASHUTOSH-13 <ashutoshkalash03@gmail.com>
2024-09-24 01:40:58 +0530

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,50 @@
+function hamburg() {
+  const navbar = document.querySelector(".dropdown");
+  navbar.style.transform = "translateY(0px)";
+}
+
+function cancel() {
+  const navbar = document.querySelector(".dropdown");
+  navbar.style.transform = "translateY(-500px)";
+}
+
+// Typewriter Effect
+const texts = ["DEVELOPER", "DESIGNER", "YOUTUBER"];
+let speed = 100;
+const textElements = document.querySelector(".typewriter-text");
+
+let textIndex = 0;
+let charcterIndex = 0;
+
+function typeWriter() {
+  if (charcterIndex < texts[textIndex].length) {
+    textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
+    charcterIndex++;
+    setTimeout(typeWriter, speed);
+  } else if (charcterIndex === texts[textIndex].length) {
+    setTimeout(eraseText, 1000); // Add a slight delay before starting to erase
+  }
+}
+
+function eraseText() {
+  if (textElements.innerHTML.length > 0) {
+    textElements.innerHTML = textElements.innerHTML.slice(0, -1);
+    setTimeout(eraseText, 50);
+  } else {
+    textIndex = (textIndex + 1) % texts.length;
+    charcterIndex = 0;
+    setTimeout(typeWriter, 500);
+  }
+}
+
+window.onload = typeWriter;
+
+// Smooth Scroll Effect for Navbar Links
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
